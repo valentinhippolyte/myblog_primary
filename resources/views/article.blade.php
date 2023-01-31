85ad85b696aa04124eb676b38ee2ca1371bbd544
Valentin Hippolyte <valenthippo@gmail.com>
2023-01-31 15:18:39 +0100

    article css

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -1,4 +1,43 @@
 @extends('base')
 @section('content')
-    <h1>{{$article->title}}</h1>
+    <!DOCTYPE html>
+
+
+<!-- w3-content defines a container for fixed size centered content, 
+and is wrapped around the whole page content, except for the footer in this example -->
+<div class="w3-content" style="max-width:1400px">
+
+<!-- Header -->
+<header class="w3-container w3-center w3-padding-32"> 
+  <h1><b>MY BLOG</b></h1>
+  <p>Welcome to the blog of <span class="w3-tag">unknown</span></p>
+</header>
+
+<!-- Grid -->
+<div class="w-row">
+
+<!-- Blog entries -->
+<div class="w3-col 18 s12">
+  <!-- Blog entry -->
+  <div class="w3-card-4 w3-margin w3-white">
+    <img src="https://cdn.pixabay.com/photo/2012/08/27/14/19/mountains-55067_1280.png" alt="Nature" style="width:100%">
+    <div class="w3-container">
+      <h3><b>{{$article->title}}</b></h3>
+      <h5>{{ $article->subtitle }}, <span class="w3-opacity">{{ $article->created_at }}</span></h5>
+    </div>
+
+    <div class="w3-container">
+      <p>{{ $article->content }}</p>
+      <div class="w3-row">
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- END GRID -->
+</div><br>
+
+<!-- END w3-content -->
+</div>
+
 @endsection
--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
     <title>Base</title>
 </head>
 <body>
