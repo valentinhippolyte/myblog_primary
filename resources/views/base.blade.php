e4de837f66cbf47b3d23e6c4027533e3ddfffa74
Valentin Hippolyte <valenthippo@gmail.com>
2023-02-01 10:30:34 +0100

    welcome css

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -7,6 +7,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+    <link rel="stylesheet" href="../css/app.css">
+
     <title>Base</title>
 </head>
 <body>
