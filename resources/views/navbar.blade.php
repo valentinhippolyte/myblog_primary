3872a18ee9d3afdb9c7a38dc617623e838ed108f
Valentin Hippolyte <valenthippo@gmail.com>
2023-02-02 15:17:39 +0100

    fix navbar text align

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,11 +1,11 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
     <a class="navbar-brand" href="{{ route('home') }}">Blog</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="navbar-nav">
+    <div class="collapse navbar-collapse" id="navbarText">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         @if(Auth::user())
         <li class="nav-item">
           <a class="nav-link" href="{{ route('users') }}">Users</a>
@@ -19,6 +19,9 @@
           <a class="nav-link" href="{{ route('article-create') }}">Create article</a>
         </li>
         @endif
+      </ul>
+      <span class="navbar-text">
+        <ul class="navbar-nav d-flex">
         @if(!Auth::user())
           <li class="nav-item">
             <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
@@ -38,8 +41,8 @@
               </form>
         </li>
         @endif
-
       </ul>
+      </span>
     </div>
   </div>
 </nav>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,12 @@
 @section('content')
     <div class="section_welcome">
         <main role="main" class="align-baseline">
-            <h1 class="cover-heading">Welcome to my blog.</h1>
-            <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <h1 class="cover-heading">Welcome to my blog !</h1>
+            <p class="lead">
+                You will find all the latest articles here. <br>
+                If you wish to create, modify or delete an article, you just have to login or register. <br>
+                Have fun !
+            </p>
             <p class="lead">
             <a href="{{ route('articles') }}" class="btn btn-lg btn-secondary">see articles</a>
             </p>
